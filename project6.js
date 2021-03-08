7a416b435050f4913e890c839aa3ba45f3f9b9eb
SandraS <alsaigsandra@gmail.com>
2021-03-08 11:03:04 +0100

    Update project6.js
    Added the phrases array.

---
+++ project6.js
@@ -0,0 +1,15 @@
+
+const qwerty = document.getElementById('qwerty');
+
+const phrase = document.getElementById('phrase');
+
+const buttonReset = document.querySelector('btn__reset');
+
+let missedGuesses = 0;
+const phrases = [
+  "w h e n n o t h i n g i s g o i n g r i g h t g o l e f t ",
+  "d o n t l o o k b a c k y o u a r e n o t g o i n g t h a t w a y ",
+  "l o v e c o n q u e r s a l l ",
+  "b e l i e v e y o u c a n a n d y o u a r e h a l f w a y t h e r e ",
+  "i l i c k e d i t s o i t s m i n e "
+];
