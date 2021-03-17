e45b55a1ed22403892ed8bd5deefa30768824ffb
SandraS <alsaigsandra@gmail.com>
2021-03-17 20:20:18 +0100

    Update project6.js

--- project6.js
+++ project6.js
@@ -47,3 +47,15 @@ function addPhraseToDisplay(arr) {
 
 addPhraseToDisplay(gamePhrase);
 //Call funcion to generate a phrase from array..
+
+function checkLetter(button) {
+  let liElements = document.querySelectorAll('.class');
+  let match = 0;
+
+  for (let i =0; i < liElements.length; i++) {
+    if (button.textContent.toLowerCase() === liElements.textContent.toLowerCase()) {
+      liElements.className = 'show';
+      match += 1;
+    }
+}
+}
